7b93da8c5a770e6af8f87dcc6322e09fb57b1224
Anastasija Jadrevska <65765747+anastasija-jadrevska@users.noreply.github.com>
2021-12-01 12:38:35 +0000

    day 1

---
+++ day_1.py
@@ -0,0 +1,28 @@
+
+def task_1(in_list):
+    count = 0
+    for i in range(0,len(in_list)-1):
+        if in_list[i]<in_list[i+1]:
+            count = count+1
+    return count
+
+
+def task_2(in_list):
+    count = 0
+    for i in range (0,len(in_list)-3):
+        sum_1 = in_list[i]+in_list[i+1]+in_list[i+2]
+        sum_2 = in_list[i+1]+in_list[i+2]+in_list[i+3]
+        if sum_1<sum_2:
+            count=count+1
+    return count
+
+
+if __name__ == '__main__':
+    int_list = []
+    with open('input1.txt') as f:
+        for line in f:
+            int_list.append(int(line))
+    print(task_2(int_list))
+
+
+
