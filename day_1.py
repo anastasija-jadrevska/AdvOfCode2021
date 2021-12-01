abef60b58bd7ca8cbd6c401c03c33ee8733ad753
Anastasija Jadrevska <65765747+anastasija-jadrevska@users.noreply.github.com>
2021-12-01 12:54:44 +0000

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -17,12 +17,25 @@ def task_2(in_list):
     return count
 
 
+def improved(in_list):
+    # booleans are cast to ints when sum'd
+    # a+b+c < b+c+d if a<d
+
+    sum_1 = sum(x < y for x, y in zip(in_list, in_list[1:]))
+    sum_2 = sum(x < y for x, y in zip(in_list, in_list[3:]))
+
+    # line for both; result = solve(data,diff)
+    solve = lambda data, diff: sum(b > a for a, b in zip(data, data[diff:]))
+
+
+
 if __name__ == '__main__':
     int_list = []
     with open('input1.txt') as f:
         for line in f:
             int_list.append(int(line))
-    print(task_2(int_list))
+    # print(task_2(int_list))
+    print(list(zip(int_list, int_list[1:])))
 
 
 
